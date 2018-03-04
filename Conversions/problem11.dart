0ad1ceb046fc935033628567f2d48c58271109ce
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-03-05 06:11:08 +0800

    2018 Updates

---
+++ Conversions/problem11.dart
@@ -0,0 +1,9 @@
+#Problem 11: Write a program that converts numbers of day to minutes.
+import "dart:io";
+
+void main() {
+ int day = 1;
+ int minutes = day * 24 * 60;
+  
+ stdout.writeln("${minutes}");  
+}
