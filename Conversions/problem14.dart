af371d22ac3062c386b309f0f0c5fd3209853dbf
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-03-05 06:12:47 +0800

    2018 Updates

---
+++ Conversions/problem14.dart
@@ -0,0 +1,8 @@
+#Problem 14: Write a program that converts lower case to upper case characters.
+import 'dart:io';
+
+void main() {
+ String str = "abc";
+
+ stdout.writeln('${str.toUpperCase()}');
+}
