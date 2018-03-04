85f59ce7cc5c3d16575b9cdb70359b939741ebb9
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-03-05 06:12:17 +0800

    2018 Updates

---
+++ Conversions/problem13.dart
@@ -0,0 +1,11 @@
+#Problem 13: Write a program that converts celsius to farenheit.
+import 'dart:io';
+
+void main() {
+ double celsius = 0.0, farenheit = 0.0;
+ stdout.write('Enter value of celsius: ');
+ celsius = double.parse(stdin.readLineSync());
+ farenheit =(9.0/5.0) * celsius + 32;
+
+ stdout.writeln('${celsius}C equals to ${farenheit}F');
+}
