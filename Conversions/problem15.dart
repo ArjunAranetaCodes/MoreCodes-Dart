b2e1c85c79cb89c8a7c005e14327f8eea97d3394
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-03-05 06:13:18 +0800

    2018 Updates

---
+++ Conversions/problem15.dart
@@ -0,0 +1,9 @@
+#Problem 15: Write a program that converts letters to ASCII.
+import 'dart:io';
+import 'dart:convert';
+
+void main() {
+ var ascii = ASCII.encode("x");
+
+ stdout.writeln('${ascii}');
+}
