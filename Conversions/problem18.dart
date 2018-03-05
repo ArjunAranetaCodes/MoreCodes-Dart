976fbd11819a541b7c0e788e9e66041a0039c586
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-03-06 05:56:10 +0800

    2018 Updates

---
+++ Conversions/problem18.dart
@@ -0,0 +1,9 @@
+//Problem 18: Write a program that converts liter to milliliter.
+import "dart:io";
+
+void main() {
+ int liter = 1;
+ int millimeter = liter * 12;
+  
+ stdout.writeln("${millimeter}");  
+}
