1b64cbf49d1c501645394e9464dab3843c6dae16
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-03-06 05:55:39 +0800

    2018 Updates

---
+++ Conversions/problem17.dart
@@ -0,0 +1,9 @@
+//Problem 17: Write a program that converts kilometer to meter.
+import "dart:io";
+
+void main() {
+ int kilometer = 1;
+ int meter = kilometer * 12;
+  
+ stdout.writeln("${meter}");  
+}
